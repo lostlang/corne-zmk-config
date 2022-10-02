ac86127bdad4d72b62ac985c144e5afaecdf6baa
Alec Lang <lostlang@icloud.com>
2022-10-02 12:10:51 +0300

    chore: test arrow postfix

--- config/nav_layer.h
+++ config/nav_layer.h
@@ -9,7 +9,7 @@
 //                         '-------+-------+-------'       '-------+-------+-------'
 
 #define NavL1 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &LEFT   &UP     &DOWN   &RIGHT  &trans
+#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &LEFT_ARROW   &UP_ARROW     &DOWN_ARROW   &RIGHT_ARROW  &trans
 #define NavL3 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
 #define NavL4                         &trans  &trans  &trans /**/ &trans  &trans  &trans
 
