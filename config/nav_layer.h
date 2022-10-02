954ffc0f55ab5f89518300151558d7b2bc4e6945
Alec Lang <lostlang@icloud.com>
2022-10-02 12:21:54 +0300

    chore: add arrow

--- config/nav_layer.h
+++ config/nav_layer.h
@@ -1,7 +1,7 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
 // |       |       |       |       |       |       |       |       |       |       |       |       |       |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
-// |       |       |       |       |       |       |       |       | LEFT  |  UP   | DOWN  | RIGHT |       |
+// |       |       |       |       |       |       |       |       |  LEFT |  DOWN |   UP  | RIGHT |       |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |       |       |       |       |       |       |       |       |       |       |       |       |       |
 // '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
@@ -9,7 +9,7 @@
 //                         '-------+-------+-------'       '-------+-------+-------'
 
 #define NavL1 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
+#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &kp LEFT  &kp DOWN  &kp UP  &kp RIGHT  &trans
 #define NavL3 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
 #define NavL4                         &trans  &trans  &trans /**/ &trans  &trans  &trans
 
