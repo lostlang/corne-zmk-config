e24d4a7bc986c1a1aff12f06c15675e5fa4ad0d2
Alec Lang <lostlang@icloud.com>
2022-10-02 12:48:48 +0300

    chore: prettier colemak layer

--- config/colemak_layer.h
+++ config/colemak_layer.h
@@ -8,8 +8,8 @@
 //                         |ESC/GUI|ENT/ALT|SPC/CTR|       |BSP/CTR| -/ALT | =/GUI |
 //                         '-------+-------+-------'       '-------+-------+-------'
 
-#define ColemakL1 &kp GRAVE  &kp Q  &kp W  &kp F  &kp P  &kp B /**/ &kp J  &kp L  &kp U  &kp Y  &kp SEMI  &kp BSLH
-#define ColemakL2 &kp TAB  &lt 2 A  &lt 1 R  &mt LSHIFT S  &lt 3 T  &kp G /**/ &kp M  &lt 4 N  &mt RSHIFT E  &kp I  &kp O  &kp SQT
-#define ColemakL3 &kp LEFT_BRACKET  &kp Z  &kp X  &kp C  &kp D  &kp V /**/ &kp K  &kp H  &kp COMMA  &kp DOT  &kp FSLH  &kp RIGHT_BRACKET
-#define ColemakL4 &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
+#define ColemakL1 &kp GRAVE         &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
+#define ColemakL2 &kp TAB           &lt 2 A  &lt 1 R  &mt LSHIFT S  &lt 3 T       &kp G           /**/ &kp M           &lt 4 N         &mt RSHIFT E    &kp I    &kp O     &kp SQT
+#define ColemakL3 &kp LEFT_BRACKET  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RIGHT_BRACKET
+#define ColemakL4                                     &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
 
