30b57b6f4dcdd47954f90ef34184ae169e7e6685
Alec Lang <lostlang@icloud.com>
2022-10-02 12:45:36 +0300

    chore: add bt

--- config/colemak_layer.h
+++ config/colemak_layer.h
@@ -1,7 +1,7 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
 // |   `   |   Q   |   W   |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
-// |  Tab  |  A/FN | R/NUM | S/SHFT| T/NAV |   G   |       |   M   |   N   | E/SHFT|   I   |   O   |   '   |
+// |  Tab  |  A/FN | R/NUM | S/SHFT| T/NAV |   G   |       |   M   |  N/BT | E/SHFT|   I   |   O   |   '   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |   [   |   Z   |   X   |   C   |   D   |   V   |       |   K   |   H   |   ,   |   .   |   /   |   ]   |
 // '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
@@ -9,7 +9,7 @@
 //                         '-------+-------+-------'       '-------+-------+-------'
 
 #define ColemakL1 &kp GRAVE  &kp Q  &kp W  &kp F  &kp P  &kp B /**/ &kp J  &kp L  &kp U  &kp Y  &kp SEMI  &kp BSLH
-#define ColemakL2 &kp TAB  &lt 2 A  &lt 1 R  &mt LSHIFT S  &lt 3 T  &kp G /**/ &kp M  &kp N  &mt RSHIFT E  &kp I  &kp O  &kp SQT
+#define ColemakL2 &kp TAB  &lt 2 A  &lt 1 R  &mt LSHIFT S  &lt 3 T  &kp G /**/ &kp M  &lt 4 N  &mt RSHIFT E  &kp I  &kp O  &kp SQT
 #define ColemakL3 &kp LEFT_BRACKET  &kp Z  &kp X  &kp C  &kp D  &kp V /**/ &kp K  &kp H  &kp COMMA  &kp DOT  &kp FSLH  &kp RIGHT_BRACKET
 #define ColemakL4 &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
 
--- config/nav_layer.h
+++ config/nav_layer.h
@@ -1,15 +1,15 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
-// |       |       |       |       |       |       |       |       |       |       |       |       |       |
+// |       |       |       |       |       |       |       | PSCRN |       |       |       |       |  DEL  |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |       |       |       |       |       |       |       |       |  LEFT |  DOWN |   UP  | RIGHT |       |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |       |       |       |       |       |       |       |       |       |       |       |       |       |
 // '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
-//                       |        |        |       |       |       |       |       |
+//                         |       |       |       |       |       |       |       |
 //                         '-------+-------+-------'       '-------+-------+-------'
 
-#define NavL1 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &kp LEFT  &kp DOWN  &kp UP  &kp RIGHT  &trans
-#define NavL3 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define NavL4                         &trans  &trans  &trans /**/ &trans  &trans  &trans
+#define NavL1 &trans  &trans  &trans  &trans  &trans  &trans /**/ &kp PSCRN  &trans    &trans    &trans  &trans     &kp DEL
+#define NavL2 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans     &kp LEFT  &kp DOWN  &kp UP  &kp RIGHT  &trans
+#define NavL3 &trans  &trans  &trans  &trans  &trans  &trans /**/ &trans     &trans    &trans    &trans  &trans     &trans
+#define NavL4                         &trans  &trans  &trans /**/ &trans     &trans    &trans
 
