8b4adad75462173e19232a0f1761ee2444e85d67
Alec Lang <lostlang@icloud.com>
2022-10-02 21:27:14 +0300

    chore: add qwerty combo

--- config/colemak_layer.h
+++ config/colemak_layer.h
@@ -8,7 +8,7 @@
 //                         |ESC/GUI|ENT/ALT|SPC/CTR|       |BSP/CTR| -/ALT | =/GUI |
 //                         '-------+-------+-------'       '-------+-------+-------'
 
-#define ColemakL1 &kp GRAVE         &tt 1 Q  &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
+#define ColemakL1 &kp GRAVE         &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
 #define ColemakL2 &kp TAB           &lt 3 A  &lt 2 R  &mt LSHIFT S  &lt 4 T       &kp G           /**/ &kp M           &lt 5 N         &mt RSHIFT E    &kp I    &kp O     &kp SQT
 #define ColemakL3 &kp LEFT_BRACKET  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RIGHT_BRACKET
 #define ColemakL4                                     &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
--- config/qwerty_layer.h
+++ config/qwerty_layer.h
@@ -8,7 +8,7 @@
 //                         |ESC/GUI|ENT/ALT|SPC/CTR|       |BSP/CTR| -/ALT | =/GUI |
 //                         '-------+-------+-------'       '-------+-------+-------'
 
-#define QwertyL1 &kp GRAVE                &tt 0 Q  &kp W    &kp E         &kp R         &kp T     /**/ &kp Y           &kp U           &kp I           &kp O    &kp P     &kp BSLH
+#define QwertyL1 &kp GRAVE                &kp Q    &kp W    &kp E         &kp R         &kp T     /**/ &kp Y           &kp U           &kp I           &kp O    &kp P     &kp BSLH
 #define QwertyL2 &kp TAB                  &kp A    &kp S    &kp D         &kp F         &kp G     /**/ &kp H           &lt 5 J         &kp K           &kp L    &kp SEMI  &kp SQT
 #define QwertyL3 &mt LSHIFT LEFT_BRACKET  &lt 3 Z  &lt 2 X  &kp C         &lt 4 V       &kp B     /**/ &kp N           &kp M           &kp COMMA       &kp DOT  &kp FSLH  &mt RSHIFT RIGHT_BRACKET
 #define QwertyL4                                            &mt LGUI ESC  &mt LALT RET  &kp SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
