d4e8294fba5f4e73dd9e2250c3c154be5ac5b690
Alec Lang <lostlang@icloud.com>
2022-10-18 12:21:26 +0300

    chore: tap dence bracket

--- config/colemak_layer.h
+++ config/colemak_layer.h
@@ -1,5 +1,5 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
-// |   `   | Q/QWE |   W   |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
+// |   `   | Q/QWE | W/QWE |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |  Tab  |  A/FN | R/NUM | S/SHFT| T/NAV |   G   |       |   M   |  N/BT | E/SHFT|I/MEDIA| O/CHAR|   '   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
@@ -8,8 +8,8 @@
 //                         |ESC/GUI|ENT/ALT|SPC/CTR|       |BSP/CTR| -/ALT | =/GUI |
 //                         '-----------------------'       '-----------------------'
 
-#define ColemakL1 &kp GRAVE         &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
-#define ColemakL2 &kp TAB           &lt 3 A  &lt 2 R  &mt LSHIFT S  &lt 4 T       &kp G           /**/ &kp M           &lt 5 N         &mt RSHIFT E    &lt 6 I  &lt 7 O   &kp SQT
-#define ColemakL3 &kp LEFT_BRACKET  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RIGHT_BRACKET
-#define ColemakL4                                     &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
+#define ColemakL1 &kp GRAVE      &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
+#define ColemakL2 &kp TAB        &lt 3 A  &lt 2 R  &mt LSHIFT S  &lt 4 T       &kp G           /**/ &kp M           &lt 5 N         &mt RSHIFT E    &lt 6 I  &lt 7 O   &kp SQT
+#define ColemakL3 &left_bracket  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &right_bracket
+#define ColemakL4                                  &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
 
