d344ff3eaf146dfd68f632784739803139ae5f50
Alec Lang <lostlang@icloud.com>
2022-10-18 12:03:58 +0300

    chore: change char layout

--- config/char_layer.h
+++ config/char_layer.h
@@ -1,14 +1,14 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
-// |       |   :   |   ;   |   &   |   '   |   !   |       |       |       |       |       |       |       |
+// |       |   !   |   $   |   &   |       |       |       |       |       |       |       |       |       |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
-// |       |   @   |   %   |   *   |   #   |   $   |       |       |       |       |       |       |       |
+// |       |   @   |   %   |   *   |   #   |       |       |       |       |       |       |       |       |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
-// |       |   (   |   )   |   ^   |   "   |   ?   |       |       |       |       |       |       |       |
+// |       |   (   |   )   |   ^   |       |       |       |       |       |       |       |       |       |
 // '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
 //                         |       |       |       |       |       |       |       |
 //                         '-----------------------'       '-----------------------'
 
-#define CharL1 &trans  &kp COLON  &kp SEMI   &kp AMPS   &kp SQT   &kp EXCL  /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define CharL2 &trans  &kp AT     &kp PRCNT  &kp STAR   &kp HASH  &kp DLLR  /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define CharL3 &trans  &kp LPAR   &kp RPAR   &kp CARET  &kp DQT   &kp QMARK /**/ &trans  &trans  &trans  &trans  &trans  &trans
-#define CharL4                               &trans     &trans    &trans    /**/ &trans  &trans  &trans
+#define CharL1 &trans  &kp EXCL  &kp DLLR   &trans    &trans    &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
+#define CharL2 &trans  &kp AT    &kp PRCNT  &kp STAR  &kp HASH  &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
+#define CharL3 &trans  &kp LPAR  &kp RPAR   &trans    &trans    &trans /**/ &trans  &trans  &trans  &trans  &trans  &trans
+#define CharL4                              &trans    &trans    &trans /**/ &trans  &trans  &trans
