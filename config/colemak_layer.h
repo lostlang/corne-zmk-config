1c8cd6b12b23f3db9955876a2ec9de4ae6089f0b
Alec Lang <lostlang@icloud.com>
2023-09-08 16:31:57 +0300

    init: new layout building

--- config/colemak_layer.h
+++ config/colemak_layer.h
@@ -1,15 +1,15 @@
 // .-----------------------------------------------.       .-----------------------------------------------.
-// |   `   | Q/QWE | W/QWE |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
+// |   `   | Q | W |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
-// |  Tab  |  A/FN | R/NUM | S/SHFT| T/NAV |   G   |       |   M   |  N/BT | E/SHFT|I/MEDIA| O/CHAR|   '   |
+// |  Tab  |  A | R | S| T |   G   |       |   M   |  N | E|I| O|   '   |
 // |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
 // |   [   |   Z   |   X   |   C   |   D   |   V   |       |   K   |   H   |   ,   |   .   |   /   |   ]   |
 // '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
-//                         |ESC/GUI|ENT/ALT|SPC/CTR|       |BSP/CTR| -/ALT | =/GUI |
+//                         |ESC|ENT|SPC|       |BSP| LSHIFT | GUI |
 //                         '-----------------------'       '-----------------------'
 
 #define ColemakL1 &kp GRAVE      &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
-#define ColemakL2 &kp TAB        &lt 3 A  &lt 2 R  &mt LSHIFT S  &lt 4 T       &kp G           /**/ &kp M           &lt 5 N         &mt RSHIFT E    &lt 6 I  &lt 7 O   &kp SQT
-#define ColemakL3 &left_bracket  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &right_bracket
-#define ColemakL4                                  &mt LGUI ESC  &mt LALT RET  &mt LCTRL SPACE /**/ &mt LCTRL BSPC  &mt LALT MINUS  &mt LGUI EQUAL
+#define ColemakL2 &kp TAB        &kp A  &kp R  &kp S  &kp T       &kp G           /**/ &kp M           &kp N         &kp E    &kp I  &kp O   &kp SQT
+#define ColemakL3 &kp LBKT  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RBKT
+#define ColemakL4                                  &kp ESC  &kp RET  &kp SPACE /**/ &kp BSPC  &kp LSHIFT  &kp GUI
 
