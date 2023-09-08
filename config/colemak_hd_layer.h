73e7419cac1e22a9a3a08567f4acb9c94f80a3d1
Alec Lang <lostlang@icloud.com>
2023-09-08 23:15:26 +0300

    fix: layout

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -11,4 +11,4 @@
 #define ColemakL1 &kp GRAVE      &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
 #define ColemakL2 &kp TAB        &kp A  &kp R  &kp S  &kp T       &kp G           /**/ &kp M           &kp N         &kp E    &kp I  &kp O   &kp SQT
 #define ColemakL3 &kp LBKT  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RBKT
-#define ColemakL4                                  &kp ESC  &kp RET  &kp SPACE /**/ &kp BSPC  &kp LSHIFT  &kp GUI
+#define ColemakL4                                  &kp ESC  &kp RET  &kp SPACE /**/ &kp BSPC  &kp LSHIFT  &kp LGUI
