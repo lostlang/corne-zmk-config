181c5915cdff99d2184c437149456fe157086dc6
Alec Lang <lostlang@icloud.com>
2023-09-08 22:56:22 +0300

    chore: colemak hd add

---
+++ config/colemak_hd_layer.h
@@ -0,0 +1,14 @@
+// .-----------------------------------------------.       .-----------------------------------------------.
+// |   `   | Q | W |   F   |   P   |   B   |       |   J   |   L   |   U   |   Y   |   ;   |   \   |
+// |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+// |  Tab  |  A | R | S| T |   G   |       |   M   |  N | E|I| O|   '   |
+// |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+// |   [   |   Z   |   X   |   C   |   D   |   V   |       |   K   |   H   |   ,   |   .   |   /   |   ]   |
+// '-----------------------+-------+-------+-------|       |-------+-------+-------+-----------------------'
+//                         |ESC|ENT|SPC|       |BSP| LSHIFT | GUI |
+//                         '-----------------------'       '-----------------------'
+
+#define ColemakL1 &kp GRAVE      &kp Q    &kp W    &kp F         &kp P         &kp B           /**/ &kp J           &kp L           &kp U           &kp Y    &kp SEMI  &kp BSLH
+#define ColemakL2 &kp TAB        &kp A  &kp R  &kp S  &kp T       &kp G           /**/ &kp M           &kp N         &kp E    &kp I  &kp O   &kp SQT
+#define ColemakL3 &kp LBKT  &kp Z    &kp X    &kp C         &kp D         &kp V           /**/ &kp K           &kp H           &kp COMMA       &kp DOT  &kp FSLH  &kp RBKT
+#define ColemakL4                                  &kp ESC  &kp RET  &kp SPACE /**/ &kp BSPC  &kp LSHIFT  &kp GUI
