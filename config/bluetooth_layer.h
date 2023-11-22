85adca9401fc5504fe42cafa93acdfceb5a26d76
Alec Lang <lostlang@icloud.com>
2023-11-23 02:17:07 +0300

    fix: add bt layer

--- config/bluetooth_layer.h
+++ config/bluetooth_layer.h
@@ -1,8 +1,8 @@
-// ------------------------------------------------------------------------------
-//| BLDR |     |     |     |     |     |   |     |     |     |     |     | BLDR |
-//| BTCLR| BT0 | BT1 | BT2 | BT3 | BT4 |   | BT4 | BT3 | BT2 | BT1 | BT0 | BTCLR|
-//|pwrOFF|NUMLK|     |     |     |     |   |     |     |     |     |     |      |
-//                   |     |     |     |   |     |     |     |
+// ----------------------------------------------------------------------------
+//|     |     |     |     |     |     |   |     |     |     |     |     |     |
+//| BTCL| BT0 | BT1 | BT2 | BT3 | BT4 |   |     |     |     |     |     |     |
+//|     |     |     |     |     |     |   |     |     |     |     |     |     |
+//                  |     |     |     |   |     |     |     |
 
 #define BluetoothL1 &none       &none         &none         &none         &none         &none        /**/ &trans  &none  &none  &none  &none  &none
 #define BluetoothL2 &bt BT_CLR  &bt BT_SEL 0  &bt BT_SEL 1  &bt BT_SEL 2  &bt BT_SEL 3  &bt BT_SEL 4 /**/ &none   &none  &none  &none  &none  &none
