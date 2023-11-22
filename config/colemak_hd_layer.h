af04e7fe1cdba7e533218b2d7b7a48efd5a0d3e5
Alec Lang <lostlang@icloud.com>
2023-11-23 02:25:03 +0300

    fix: layer id

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -7,7 +7,7 @@
 //      hold         | LGUI| LSFT|     |   |     |     |     |
 
 
-#define ColemakL1 &kp GRAVE  &kp Q  &kp W  &kp F         &kp P           &kp B     /**/ &lt 2 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
+#define ColemakL1 &kp GRAVE  &kp Q  &kp W  &kp F         &kp P           &kp B     /**/ &lt 1 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
 #define ColemakL2 &kp TAB    &kp A  &kp R  &kp S         &kp T           &kp G     /**/ &kp M      &kp N     &kp E      &kp I    &kp O     &td_lbr
 #define ColemakL3 &kp SQT    &kp Z  &kp X  &kp C         &kp D           &kp V     /**/ &kp K      &kp H     &kp COMMA  &kp DOT  &kp SEMI  &td_rbr
 #define ColemakL4                          &mt LGUI ESC  &mt LSHIFT RET  &kp SPACE /**/ &kp LCTRL  &kp LALT  &kp BSPC
