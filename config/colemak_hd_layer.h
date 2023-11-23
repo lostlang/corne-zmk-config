37c6137a4fb4279aa5c0ee426964d4de2f61c9b3
Alec Lang <lostlang@icloud.com>
2023-11-23 03:00:03 +0300

    chore: tapdance delay 180 and esc move

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -1,13 +1,12 @@
 // -----------------------------------------------------------------------------
-// |  `  |  Q  |  W  |  F  |  P  |  B  |   | J BT|  L  |  U  |  Y  |  /  |  \  |
+// |` ESC|  Q  |  W  |  F  |  P  |  B  |   | J BT|  L  |  U  |  Y  |  /  |  \  |
 // | Tab |  A  |  R  |  S  |  T  |  G  |   |  M  |  N  |  E  |  I  |  O  | [ ( |
 // |  '  |  Z  |  X  |  C  |  D  |  V  |   |  K  |  H  |  ,  |  .  |  ;  | ] ) |
-//       tap         | ESC | ENT | SPC |   | LCTR| LALT| BSPC|
-//                   +-----+-----+-----+---+-----+-----+-----+
-//      hold         | LGUI| LSFT|     |   |     |     |     |
+//       tap         | LGUI| ENT | SPC |   | LCTR| LALT| BSPC|
+//      hold         |     | LSFT|     |   |     |     |     |
 
 
-#define ColemakL1 &kp GRAVE  &kp Q  &kp W  &kp F         &kp P           &kp B     /**/ &lt 1 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
-#define ColemakL2 &kp TAB    &kp A  &kp R  &kp S         &kp T           &kp G     /**/ &kp M      &kp N     &kp E      &kp I    &kp O     &td_lbr
-#define ColemakL3 &kp SQT    &kp Z  &kp X  &kp C         &kp D           &kp V     /**/ &kp K      &kp H     &kp COMMA  &kp DOT  &kp SEMI  &td_rbr
-#define ColemakL4                          &mt LGUI ESC  &mt LSHIFT RET  &kp SPACE /**/ &kp LCTRL  &kp LALT  &kp BSPC
+#define ColemakL1 &td_esc  &kp Q  &kp W  &kp F     &kp P           &kp B     /**/ &lt 1 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
+#define ColemakL2 &kp TAB  &kp A  &kp R  &kp S     &kp T           &kp G     /**/ &kp M      &kp N     &kp E      &kp I    &kp O     &td_lbr
+#define ColemakL3 &kp SQT  &kp Z  &kp X  &kp C     &kp D           &kp V     /**/ &kp K      &kp H     &kp COMMA  &kp DOT  &kp SEMI  &td_rbr
+#define ColemakL4                        &mt LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp LCTRL  &kp LALT  &kp BSPC
