16e6c657400a9278f074f56b32fb39828a815bef
Alec Lang <lostlang@icloud.com>
2023-12-11 23:12:59 +0300

    chore: lalt

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -9,4 +9,4 @@
 #define ColemakL1 &td_esc  &kp Q  &kp W  &kp F     &kp P           &kp B     /**/ &lt 1 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
 #define ColemakL2 &kp TAB  &kp A  &kp R  &kp S     &kp T           &kp G     /**/ &kp M      &kp N     &kp E      &kp I    &kp O     &td_lbr
 #define ColemakL3 &kp SQT  &kp Z  &kp X  &kp C     &kp D           &kp V     /**/ &kp K      &kp H     &kp COMMA  &kp DOT  &kp SEMI  &td_rbr
-#define ColemakL4                        &kp LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp LCTRL  &kp LALT  &kp BSPC
+#define ColemakL4                        &kp LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp LALT  &kp LALT  &kp BSPC
