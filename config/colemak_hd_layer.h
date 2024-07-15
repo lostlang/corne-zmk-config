949913ad2e453952c41b6c32eb452278222f46bc
Alec Lang <lostlang@icloud.com>
2024-07-15 06:45:51 +0000

    feat: update layout

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -1,12 +1,12 @@
 // -----------------------------------------------------------------------------
 // |` ESC|  Q  |  W  |  F  |  P  |  B  |   | J BT|  L  |  U  |  Y  |  /  |  \  |
-// | Tab |  A  |  R  |  S  |  T  |  G  |   |  M  |  N  |  E  |  I  |  O  | [ ( |
-// |  '  |  Z  |  X  |  C  |  D  |  V  |   |  K  |  H  |  ,  |  .  |  ;  | ] ) |
-//       tap         | LGUI| ENT | SPC |   | LCTR| LALT| BSPC|
+// |  ;  |  A  |  R  |  S  |  T  |  G  |   |  M  |  N  |  E  |  I  |  O  | [ ( |
+// |  '  |  Z  |  X  |  C  |  D  | TAB |   |  V  |  K  |  H  |  ,  |  .  | ] ) |
+//       tap         | LGUI| ENT | SPC |   | BSPC| LCTR| LALT|
 //      hold         |     | LSFT|     |   |     |     |     |
 
 
-#define ColemakL1 &td_esc  &kp Q  &kp W  &kp F     &kp P           &kp B     /**/ &lt 1 J    &kp L     &kp U      &kp Y    &kp FSLH  &kp BSLH
-#define ColemakL2 &kp TAB  &kp A  &kp R  &kp S     &kp T           &kp G     /**/ &kp M      &kp N     &kp E      &kp I    &kp O     &td_lbr
-#define ColemakL3 &kp SQT  &kp Z  &kp X  &kp C     &kp D           &kp V     /**/ &kp K      &kp H     &kp COMMA  &kp DOT  &kp SEMI  &td_rbr
-#define ColemakL4                        &kp LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp LALT  &kp LALT  &kp BSPC
+#define ColemakL1 &td_esc   &kp Q  &kp W  &kp F     &kp P           &kp B     /**/ &lt 1 J   &kp L     &kp U     &kp Y      &kp FSLH  &kp BSLH
+#define ColemakL2 &kp SEMI  &kp A  &kp R  &kp S     &kp T           &kp G     /**/ &kp M     &kp N     &kp E     &kp I      &kp O     &td_lbr
+#define ColemakL3 &kp SQT   &kp Z  &kp X  &kp C     &kp D           &kp TAB   /**/ &kp V     &kp K     &kp H     &kp COMMA  &kp DOT   &td_rbr
+#define ColemakL4                         &kp LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp BSPC  &kp LCTR  &kp LALT
