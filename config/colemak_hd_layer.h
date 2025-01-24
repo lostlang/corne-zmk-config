618a04f16dd1eef32dacfc7961990dd1acfcbca7
Alec Lang <lostlang@icloud.com>
2025-01-24 21:28:44 +0500

    feat: use homerow

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -1,12 +1,11 @@
 // -----------------------------------------------------------------------------
-// |` ESC|  Q  |  W  |  F  |  P  |  B  |   | J BT|  L  |  U  |  Y  |  /  |  \  |
-// |  ;  |  A  |  R  |  S  |  T  |  G  |   |  M  |  N  |  E  |  I  |  O  | [ ( |
-// |  '  |  Z  |  X  |  C  |  D  | TAB |   |  V  |  K  |  H  |  ,  |  .  | ] ) |
-//       tap         | LGUI| ENT | SPC |   | BSPC| LCTR| LALT|
-//      hold         |     | LSFT|     |   |     |     |     |
+// |` ESC|  Q   |  W  |   F   |  P   |  B  |   | J BT|  L   |   U   |  Y  |  /   |  \  |
+// |  ;  |A META|R ALT|S SHIFT|T CTRL|  G  |   |  M  |N CTRL|E SHIFT|I ALT|O META| [ ( |
+// |  '  | TAB  |  Z  |   X   |  C   |  D  |   |  V  |  K   |   H   |  ,  |  .   | ] ) |
+//                    |  ENT  | SPC  |  -  |   |  -  | BSPC |  DEL  |
 
 
-#define ColemakL1 &td_esc   &kp Q    &kp W  &kp F     &kp P           &kp B     /**/ &lt 1 J   &kp L      &kp U     &kp Y      &kp FSLH  &kp BSLH
-#define ColemakL2 &kp SEMI  &kp A    &kp R  &kp S     &kp T           &kp G     /**/ &kp M     &kp N      &kp E     &kp I      &kp O     &td_lbr
-#define ColemakL3 &kp SQT   &kp TAB  &kp Z  &kp X     &kp C           &kp D     /**/ &kp V     &kp K      &kp H     &kp COMMA  &kp DOT   &td_rbr
-#define ColemakL4                           &kp LGUI  &mt LSHIFT RET  &kp SPACE /**/ &kp BSPC  &kp LCTRL  &kp LALT
+#define ColemakL1 &td_esc   &kp Q       &kp W       &kp F         &kp P        &kp B  /**/  &lt 1 J  &kp L        &kp U         &kp Y       &kp FSLH    &kp BSLH
+#define ColemakL2 &kp SEMI  &mt LGUI A  &mt LALT R  &mt LSHIFT S  &mt LCTRL T  &kp G  /**/  &kp M    &mt RCTRL N  &mt RSHIFT E  &mt RALT I  &mt RGUI O  &td_lbr
+#define ColemakL3 &kp SQT   &kp TAB     &kp Z       &kp X         &kp C        &kp D  /**/  &kp V    &kp K        &kp H         &kp COMMA   &kp DOT     &td_rbr
+#define ColemakL4                                   &kp RET       &kp SPACE    &none  /**/  &none    &kp BSPC     &kp DEL
