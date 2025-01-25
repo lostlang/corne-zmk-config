3eab666922027e8cd32d6280768d4f3719914dc1
Alec Lang <lostlang@icloud.com>
2025-01-25 14:29:16 +0500

    fix: build error

--- config/arrow_layer.h
+++ config/arrow_layer.h
@@ -5,7 +5,7 @@
 //                   |     |     |     |   |     |     |       |
 
 
-#define ArrowL1 &none  &none  &none  &none   &none  &none  /**/ &none  &HOME  &PG_DN  &PG_UP  &END    &none
-#define ArrowL2 &none  &none  &none  &none   &none  &none  /**/ &none  &LEFT  &DOWN   &UP     &RIGHT  &none
-#define ArrowL3 &none  &none  &none  &none   &none  &none  /**/ &none  &none  &none   &none   &none   &none
-#define ArrowL4                      &trans  &none  &none  /**/ &none  &none  &none
+#define ArrowL1 &none  &none  &none  &none   &none  &none  /**/ &none  &kp HOME  &kp PG_DN  &kp PG_UP  &kp END    &none
+#define ArrowL2 &none  &none  &none  &none   &none  &none  /**/ &none  &kp LEFT  &kp DOWN   &kp UP     &kp RIGHT  &none
+#define ArrowL3 &none  &none  &none  &none   &none  &none  /**/ &none  &none     &none      &none      &none      &none
+#define ArrowL4                      &trans  &none  &none  /**/ &none  &none     &none
