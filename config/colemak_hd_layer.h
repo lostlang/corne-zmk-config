b802728bbc3f5c584b38b627ee79ddf4e560bad4
Alec Lang <lostlang@icloud.com>
2025-01-25 14:21:49 +0500

    chore: serch error

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -8,4 +8,4 @@
 #define ColemakL1 &td_esc   &kp Q       &kp W       &kp F         &kp P        &kp B   /**/  &lt 1 J   &kp L        &kp U         &kp Y       &kp FSLH    &kp BSLH
 #define ColemakL2 &kp SEMI  &mt LGUI A  &mt LALT R  &mt LSHIFT S  &mt LCTRL T  &kp G   /**/  &kp M     &mt RCTRL N  &mt RSHIFT E  &mt RALT I  &mt RGUI O  &td_lbr
 #define ColemakL3 &kp SQT   &kp TAB     &kp Z       &kp X         &kp C        &kp D   /**/  &kp V     &kp K        &kp H         &kp COMMA   &kp DOT     &td_rbr
-#define ColemakL4                                   &mo 2         &kp RET      &SPACE  /**/  &kp BSPC  &kp DEL      &none
+#define ColemakL4                                   &none         &kp RET      &SPACE  /**/  &kp BSPC  &kp DEL      &none
