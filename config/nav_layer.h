5b5884cc7953b770670177693bb782ec0c45b66d
Alec Lang <lostlang@icloud.com>
2025-01-28 13:49:45 +0500

    chore: rename layer, fix style comment keyboard

---
+++ config/nav_layer.h
@@ -0,0 +1,11 @@
+// -------------------------------------   ---------------------------------------
+// |     |     |     |     |     |     |   |     | HOME|PG DOWN|PG UP| END |     |
+// |     | META| ALT |SHIFT| CTRL|     |   |     | <-  |   ⬇   |  ⬆  |  -> |     |
+// |     |     |     |     |     |     |   | ⏯   | ⏮   |   🔉  |  🔊 |  ⏭  | 🔇  |
+//                   |     |     |     |   |     |     |       |
+
+
+#define NavL1 &none  &none     &none     &none       &none      &none  /**/ &none     &kp HOME    &kp PG_DN     &kp PG_UP     &kp END     &none
+#define NavL2 &none  &kp LGUI  &kp LALT  &kp LSHIFT  &kp LCTRL  &none  /**/ &none     &kp LEFT    &kp DOWN      &kp UP        &kp RIGHT   &none
+#define NavL3 &none  &none     &none     &none       &none      &none  /**/ &kp C_PP  &kp C_PREV  &kp C_VOL_DN  &kp C_VOL_UP  &kp C_NEXT  &kp C_MUTE
+#define NavL4                            &trans      &none      &none  /**/ &none     &none       &none
