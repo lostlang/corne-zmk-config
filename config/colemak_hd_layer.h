3c92f702ee363e679311a15e09c05f6406782464
Alec Lang <lostlang@icloud.com>
2025-01-28 15:16:35 +0500

    feat: add numbers, add combos

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -1,11 +1,11 @@
 // -----------------------------------------   -----------------------------------------
 // |` ESC|  Q   |  W  |   F   |  P   |  B  |   | J BT|  L   |   U   |  Y  |  /   |  \  |
-// |  ;  |A META|R ALT|S SHIFT|T CTRL|  G  |   |  M  |N CTRL|E SHIFT|I ALT|O META| [ ( |
-// |  '  | TAB  |  Z  |   X   |  C   |  D  |   |  V  |  K   |   H   |  ,  |  .   | ] ) |
-//                    | ARROW | ENT  |SPACE|   | BSPC|  DEL |   -   |
+// |  =  |A META|R ALT|S SHIFT|T CTRL|  G  |   |  M  |N CTRL|E SHIFT|I ALT|O META|  -  |
+// |  '  | TAB  |  Z  |   X   |  C   |  D  |   |  V  |  K   |   H   |  ,  |  .   |  ;  |
+//                    |  NAV  | ENT  |SPACE|   | BSPC|  DEL |   -   |
 
 
-#define ColemakL1 &td_esc   &kp Q       &kp W       &kp F         &kp P        &kp B      /**/  &lt 1 J   &kp L        &kp U         &kp Y       &kp FSLH    &kp BSLH
-#define ColemakL2 &kp SEMI  &mt LGUI A  &mt LALT R  &mt LSHIFT S  &mt LCTRL T  &kp G      /**/  &kp M     &mt RCTRL N  &mt RSHIFT E  &mt RALT I  &mt RGUI O  &td_lbr
-#define ColemakL3 &kp SQT   &kp TAB     &kp Z       &kp X         &kp C        &kp D      /**/  &kp V     &kp K        &kp H         &kp COMMA   &kp DOT     &td_rbr
-#define ColemakL4                                   &mo 2         &kp RET      &kp SPACE  /**/  &kp BSPC  &kp DEL      &none
+#define ColemakL1 &td_esc    &kp Q       &kp W       &kp F         &kp P        &kp B      /**/  &lt 1 J   &kp L        &kp U         &kp Y       &kp FSLH    &kp BSLH
+#define ColemakL2 &kp EQUAL  &mt LGUI A  &mt LALT R  &mt LSHIFT S  &mt LCTRL T  &kp G      /**/  &kp M     &mt RCTRL N  &mt RSHIFT E  &mt RALT I  &mt RGUI O  &kp MINUS
+#define ColemakL3 &kp SQT    &kp TAB     &kp Z       &kp X         &kp C        &kp D      /**/  &kp V     &kp K        &kp H         &kp COMMA   &kp DOT     &kp SEMI
+#define ColemakL4                                    &mo 2         &kp RET      &kp SPACE  /**/  &kp BSPC  &kp DEL      &mo 3
