b4c8095573a22cdc330f35630088a24bbbf91871
Alec Lang <lostlang@icloud.com>
2025-02-19 22:58:26 +0500

    feat: add fn layer

---
+++ config/fn_layer.h
@@ -0,0 +1,11 @@
+// -------------------------------------   -------------------------------------
+// |     |     | ALT |SHIFT| CTRL|     |   | F16 |  F1 |  F4 |  F7 | F10 | F13 |
+// |     |     |     |     |     |     |   | F17 |  F2 |  F5 |  F8 | F11 | F14 |
+// |     |     |     |     |     |     |   | F18 |  F3 |  F6 |  F9 | F12 | F15 |
+//                   |     |     |     |   |     |     |     |
+
+
+#define FnL1 &none  &trans  &kp LALT  &kp LSHIFT  &kp LCTRL  &none  /**/ &kp F16  &kp F1  &kp F4  &kp F7  &kp F10  &kp F13
+#define FnL2 &none  &none   &none     &none       &none      &none  /**/ &kp F17  &kp F2  &kp F5  &kp F8  &kp F11  &kp F14
+#define FnL3 &none  &none   &none     &none       &none      &none  /**/ &kp F18  &kp F3  &kp F6  &kp F9  &kp F12  &kp F15
+#define FnL4                          &none       &none      &none  /**/ &none    &none   &none
