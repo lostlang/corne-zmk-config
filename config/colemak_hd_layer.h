333898d8cb056430e897a355448ba623e487d865
Alec Lang <lostlang@icloud.com>
2025-03-05 10:50:50 +0500

    feat: swap d and v

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -1,11 +1,11 @@
 // -----------------------------------------   -----------------------------------------
 // | ESC | Q FN |  W  |   F   |  P   |  B  |   | J BT|  L   |   U   |  Y  |  /   |  \  |
 // | TAB |A META|R ALT|S SHIFT|T CTRL|  G  |   |  M  |N CTRL|E SHIFT|I ALT|O META|  ;  |
-// |     |  =   |  Z  |   X   |  C   |  D  |   |  V  |  K   |   H   |  ,  |  .   |  -  |
+// |     |  =   |  Z  |   X   |  C   |  V  |   |  D  |  K   |   H   |  ,  |  .   |  -  |
 //                    |  NAV  | ENT  |SPACE|   | BSPC|  DEL |  NUM  |
 
 
 #define ColemakL1 &kp ESC  &lt 4 Q     &kp W       &kp F         &kp P        &kp B      /**/  &lt 1 J   &kp L        &kp U         &kp Y       &kp FSLH    &kp BSLH
 #define ColemakL2 &kp TAB  &mt LGUI A  &mt LALT R  &mt LSHIFT S  &mt LCTRL T  &kp G      /**/  &kp M     &mt RCTRL N  &mt RSHIFT E  &mt RALT I  &mt RGUI O  &kp SEMI
-#define ColemakL3 &none    &kp EQUAL   &kp Z       &kp X         &kp C        &kp D      /**/  &kp V     &kp K        &kp H         &kp COMMA   &kp DOT     &kp MINUS
+#define ColemakL3 &none    &kp EQUAL   &kp Z       &kp X         &kp C        &kp V      /**/  &kp D     &kp K        &kp H         &kp COMMA   &kp DOT     &kp MINUS
 #define ColemakL4                                  &mo 2         &kp RET      &kp SPACE  /**/  &kp BSPC  &kp DEL      &mo 3
