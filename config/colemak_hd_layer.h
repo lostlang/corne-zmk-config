aad32929de3e0f5b1d7778fde573154e3de034ff
Alec Lang <lostlang@icloud.com>
2025-11-09 16:03:08 +0500

    chore: try another f

--- config/colemak_hd_layer.h
+++ config/colemak_hd_layer.h
@@ -6,6 +6,6 @@
 
 
 #define ColemakL1 &kp ESC  &lt 4 Q    &kp W      &kp F         &kp P      &kp B      /**/  &lt 1 J   &kp L      &kp U         &kp Y      &kp FSLH   &kp BSLH
-#define ColemakL2 &kp TAB  &mt F13 A  &mt F14 R  &mt LSHIFT S  &mt F15 T  &kp G      /**/  &kp M     &mt F15 N  &mt LSHIFT E  &mt F14 I  &mt F13 O  &kp SEMI
+#define ColemakL2 &kp TAB  &mt F13 A  &mt F14 R  &mt LSHIFT S  &mt F15 T  &kp G      /**/  &kp M     &mt F15 N  &mt LSHIFT E  &mt F14 I  &mt F12 O  &kp SEMI
 #define ColemakL3 &none    &kp EQUAL  &kp Z      &kp X         &kp C      &kp V      /**/  &kp D     &kp K      &kp H         &kp COMMA  &kp DOT    &kp MINUS
 #define ColemakL4                                &mo 2         &kp RET    &kp SPACE  /**/  &kp BSPC  &kp DEL    &mo 3
